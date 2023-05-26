06938b1fb2b24599831ff4e20c460bfb2bb23259
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-26 19:47:57 +0530

    Update chitti_the_robot.py

--- chitti_the_robot.py
+++ chitti_the_robot.py
@@ -62,63 +62,63 @@ with open('intents.json') as file:
 
 # Create the main app interface
 def app():
+    # Initialize the chat history
+    if 'chat_history' not in st.session_state:
+        st.session_state['chat_history'] = []
 
+    # Function to display a chat message
+    def display_chat_message(role, message):
+        if role == 'user':
+            st.write(':smiley:', message)
+        elif role == 'bot':
+            st.write(':robot_face:', message)
 
-# Initialize the chat history
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history'] = []
+    # Display the chat interface
+    st.title('Chat Application', style='text-align: center;')
 
-# Function to display a chat message
-def display_chat_message(role, message):
-    if role == 'user':
-        st.write(':smiley:', message)
-    elif role == 'bot':
-        st.write(':robot_face:', message)
+    # Calculate the width ratio for the layout
+    sidebar_width = 4
+    content_width = 12 - sidebar_width
 
-# Display the chat interface
-st.title('Chat Application', style='text-align: center;')
+    # Create a two-column layout
+    col1, col2 = st.beta_columns([sidebar_width, content_width])
 
+    # Center the chat application
+    col1.empty()
+    col2.text("")
 
-# Calculate the width ratio for the layout
-sidebar_width = 4
-content_width = 12 - sidebar_width
+    # Sidebar for user input and send button
+    with col1:
+        st.header('User Input')
+        user_input = st.text_input('Type your message here')
 
-# Create a two-column layout
-col1, col2 = st.beta_columns([sidebar_width, content_width])
+        # Move the send button next to the input box
+        col1.text("")
+        col1.text("")
+        send_button = col1.button('Send')
 
-# Center the chat application
-col1.empty()
-col2.text("")
+    # Main content area for chat history
+    with col2:
+        st.header('Chat History')
+        for role, message in st.session_state['chat_history']:
+            display_chat_message(role, message)
 
-# Sidebar for user input and send button
-with col1:
-    st.header('User Input')
-    user_input = st.text_input('Type your message here')
+    # Process user input and generate response
+    if send_button:
+        # Get user input
+        user_message = user_input.strip()
 
-    # Move the send button next to the input box
-    col1.text("")
-    col1.text("")
-    send_button = col1.button('Send')
+        # Add user message to chat history
+        st.session_state['chat_history'].append(('user', user_message))
 
-# Main content area for chat history
-with col2:
-    st.header('Chat History')
-    for role, message in st.session_state['chat_history']:
-        display_chat_message(role, message)
+        # Generate bot response
+        bot_response = chatbot_response(user_message)
 
-# Process user input and generate response
-if send_button:
-    # Get user input
-    user_message = user_input.strip()
+        # Add bot response to chat history
+        st.session_state['chat_history'].append(('bot', bot_response))
 
-    # Add user message to chat history
-    st.session_state['chat_history'].append(('user', user_message))
+        # Clear user input
+        user_input = ''
 
-    # Generate bot response
-    bot_response = chatbot_response(user_message)
-
-    # Add bot response to chat history
-    st.session_state['chat_history'].append(('bot', bot_response))
-
-    # Clear user input
-    user_input = ''
+if __name__ == '__main__':
+    app()
