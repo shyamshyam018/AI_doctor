d3dcb3009ae17fee4498f190efe937efb46f6f3a
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-26 19:38:59 +0530

    Update chitti_the_robot.py

--- chitti_the_robot.py
+++ chitti_the_robot.py
@@ -65,53 +65,49 @@ def app():
     st.title("Chatbot")
     st.write("Welcome! Start a conversation by typing in the message box below.")
 
-    # Initialize the chat history
-    if 'chat_history' not in st.session_state:
-        st.session_state['chat_history'] = []
+    import streamlit as st
 
-    # Function to display a chat message
-    def display_chat_message(role, message):
-        if role == 'user':
-            st.write(':smiley:', message)
-        elif role == 'bot':
-            st.write(':robot_face:', message)
+# Initialize the chat history
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
 
-    # Display the chat interface
-    st.title('Chat Application')
+# Function to display a chat message
+def display_chat_message(role, message):
+    if role == 'user':
+        st.write(':smiley:', message)
+    elif role == 'bot':
+        st.write(':robot_face:', message)
 
-    # Create a two-column layout
-    col1, col2 = st.beta_columns([4, 7])
+# Display the chat interface
+st.title('Chat Application')
 
-    # Sidebar for user input
-    with col1:
-        st.header('User Input')
-        user_input = st.text_input('Type your message here')
+# Create a two-column layout
+col1, col2 = st.beta_columns([7, 4])
 
-    # Main content area for chat history
-    with col2:
-        st.header('Chat History')
-        chat_history = st.empty()
-
-    # Process user input and generate response
-    if st.button('Send'):
-        # Get user input
-        user_message = user_input.strip()
-
-        # Add user message to chat history
-        st.session_state['chat_history'].append(('user', user_message))
-
-        # Generate bot response
-        bot_response = chatbot_response(user_message)
-
-        # Add bot response to chat history
-        st.session_state['chat_history'].append(('bot', bot_response))
-
-        # Clear user input
-        user_input = ''
-
-    # Display chat history
+# Main content area for chat history
+with col1:
+    st.header('Chat History')
     for role, message in st.session_state['chat_history']:
         display_chat_message(role, message)
 
-if __name__ == '__main__':
-    app()
+# Sidebar for user input
+with col2:
+    st.header('User Input')
+    user_input = st.text_input('Type your message here')
+
+# Process user input and generate response
+if st.button('Send'):
+    # Get user input
+    user_message = user_input.strip()
+
+    # Add user message to chat history
+    st.session_state['chat_history'].append(('user', user_message))
+
+    # Generate bot response
+    bot_response = chatbot_response(user_message)
+
+    # Add bot response to chat history
+    st.session_state['chat_history'].append(('bot', bot_response))
+
+    # Clear user input
+    user_input = ''
